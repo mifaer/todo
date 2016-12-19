0855f25f08f4bcae688e2d160a193e535a8e49e3
mifaer <mifaer@gmail.com>
2016-12-19 15:39:31 +0200

    first

---
+++ js/todo.js
@@ -0,0 +1,39 @@
+let todoList = [];
+let list = document.querySelector(".container > .collection");
+
+addBtn.addEventListener ('click', (event) => 
+    {let valueTask = document.querySelector("#textarea1").value;
+    console.log(valueTask);
+    
+    console.log(todoList);
+    
+    console.log(list);
+    let id = new Date().getTime();
+    let newTaskA = document.createElement('a');
+    newTaskA.className = "collection-item";
+    let newTaskForm = document.createElement('form');
+    let newTaskInput = document.createElement('input');
+    newTaskInput.type = "checkbox";
+    newTaskInput.setAttribute(`id`, id);
+    let newTaskLabel = document.createElement('label');
+    newTaskLabel.type = "checkbox";
+    newTaskLabel.setAttribute(`for`, id);
+    newTaskLabel.innerText = valueTask;
+    let newTaskIcon = document.createElement('i');
+    newTaskIcon.className = "small material-icons right";
+    newTaskIcon.innerText = "delete";
+    
+    newTaskForm.appendChild(newTaskInput);
+    newTaskForm.appendChild(newTaskLabel);
+    newTaskForm.appendChild(newTaskIcon);
+    console.log(newTaskForm);
+    newTaskA.append(newTaskForm);
+    list.appendChild(newTaskA);
+    
+    event.preventDefault();
+    todoList.push(newTaskA);
+    console.log(newTaskA);
+    document.querySelector("#textarea1").value = "";
+    });
+
+// list.addEventListener ('click', (event) =>
