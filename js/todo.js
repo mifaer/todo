f4ce0631ddec0aabf159d16fd206d51be392ab53
mifaer <mifaer@gmail.com>
2016-12-20 17:55:49 +0200

    refactoring code_2

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,22 @@
 (function () {
     let todoList = [];
     let list = document.querySelector('.container > .collection');
-
-    addBtn.addEventListener('click', (event) => {
+    let newTaskBtn = document.querySelector('#addBtn');
+    newTaskBtn.addEventListener('click', (event) => {
         let valueTask = document.querySelector('#textarea1').value;
 
         let id = new Date().getTime();
         let newTaskA = document.createElement('a');
-        newTaskA.className = `collection-item ${id}`;
+
+        newTaskA.className = `collection-item id-${id}`;
         let newTaskForm = document.createElement('form');
         let newTaskInput = document.createElement('input');
+
         newTaskInput.type = 'checkbox';
-        newTaskInput.setAttribute('id', `${id}`);
+        newTaskInput.setAttribute('id', `id-${id}`);
         let newTaskLabel = document.createElement('label');
         newTaskLabel.className = 'teal-text';
-        newTaskLabel.setAttribute('for', `${id}`);
+        newTaskLabel.setAttribute('for', `id-${id}`);
         newTaskLabel.innerText = valueTask;
         let newTaskIcon = document.createElement('i');
         newTaskIcon.className = 'small material-icons right';
@@ -25,12 +27,12 @@
         newTaskForm.appendChild(newTaskLabel);
         newTaskForm.appendChild(newTaskIcon);
 
-        newTaskA.append(newTaskForm);
+        newTaskA.appendChild(newTaskForm);
         list.appendChild(newTaskA);
 
         event.preventDefault();
         todoList.push(newTaskA);
-
+        counter();
         document.querySelector('#textarea1').value = '';
 
 
@@ -38,25 +40,37 @@
 
     list.addEventListener('click', (event) => {
         if (event.target.tagName === 'LABEL') {
-            if (event.target.style.textDecoration === 'line-through') {
-                event.target.style = 'text-decoration:none';
-            } else {
-                event.target.style = 'text-decoration:line-through';
-            }
-        }
-        ;
-
-        if (event.target.tagName === 'I') {
-            for (let key in todoList) {
-                for (let jey in todoList[key]) {
-                    if (event.target.parentNode.parentNode.className.split(" ")[1] === todoList[key].className.split(" ")[1]) {
-                        delete todoList[key];
-                        let elem = event.target.parentNode.parentNode;
-                        elem.remove();
-                    }
-                }
-            }
+            toggleToDo();
+        } else if (event.target.tagName === 'I') {
+            deleteToDo();
         }
     });
+    function toggleToDo() {
+        if (event.target.style.textDecoration === 'line-through') {
+            return event.target.style = 'text-decoration:none';
+        } else {
+            return event.target.style = 'text-decoration:line-through';
+        }
+    }
+    function deleteToDo() {
+        for (let key in todoList) {
+            let s = todoList[key].className.split(" ");
+            let id = s.find(item => {
+                if (item.indexOf('id') !== -1)
+                    return true;
+            });
 
+            if (event.target.parentNode.parentNode.className.split(" ").includes(id)) {
+                todoList.splice(key, 1);
+                counter();
+                let elem = event.target.parentNode.parentNode;
+                elem.remove();
+            }
+        }
+    }
+    function counter() {
+        let taskCounter = document.querySelector('.container > .title');
+        let tasks = (todoList.length == 1)? 'task': 'tasks';
+        taskCounter.innerText = `MY TODO LIST !!! (${todoList.length} ${tasks})`;
+    }
 })();
