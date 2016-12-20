6b21e71424a13ab28e969ce03020cb92d6245730
mifaer <mifaer@gmail.com>
2016-12-20 14:33:11 +0200

    Code cleaning and cursor.pointer on Delete

--- js/todo.js
+++ js/todo.js
@@ -1,68 +1,67 @@
-let todoList = [];
-let list = document.querySelector(".container > .collection");
+(function () {
+    let todoList = [];
+    let list = document.querySelector('.container > .collection');
 
-addBtn.addEventListener ('click', (event) => {
-    let valueTask = document.querySelector("#textarea1").value;
-    // console.log(valueTask);
-    // console.log(todoList);
-    // console.log(list);
-    let id = new Date().getTime();
-    let newTaskA = document.createElement('a');
-    newTaskA.className = `collection-item ${id}`;
-    let newTaskForm = document.createElement('form');
-    let newTaskInput = document.createElement('input');
-    newTaskInput.type = "checkbox";
-    newTaskInput.setAttribute(`id`, id);
-    let newTaskLabel = document.createElement('label');
-    newTaskLabel.className = "teal-text";
-    newTaskLabel.setAttribute(`for`, id);
-    newTaskLabel.innerText = valueTask;
-    let newTaskIcon = document.createElement('i');
-    newTaskIcon.className = "small material-icons right";
-    newTaskIcon.innerText = "delete";
+    addBtn.addEventListener('click', (event) => {
+        let valueTask = document.querySelector('#textarea1').value;
 
-    newTaskForm.appendChild(newTaskInput);
-    newTaskForm.appendChild(newTaskLabel);
-    newTaskForm.appendChild(newTaskIcon);
-    // console.log(newTaskForm);
-    newTaskA.append(newTaskForm);
-    list.appendChild(newTaskA);
+        let id = new Date().getTime();
+        let newTaskA = document.createElement('a');
+        newTaskA.className = `collection-item ${id}`;
+        let newTaskForm = document.createElement('form');
+        let newTaskInput = document.createElement('input');
+        newTaskInput.type = 'checkbox';
+        newTaskInput.setAttribute(`id`, id);
+        let newTaskLabel = document.createElement('label');
+        newTaskLabel.className = 'teal-text';
+        newTaskLabel.setAttribute(`for`, id);
+        newTaskLabel.innerText = valueTask;
+        let newTaskIcon = document.createElement('i');
+        newTaskIcon.className = 'small material-icons right';
+        newTaskIcon.style.cursor = 'pointer';
+        newTaskIcon.innerText = 'delete';
 
-    event.preventDefault();
-    todoList.push(newTaskA);
-    // console.log(newTask A);
-    document.querySelector("#textarea1").value = "";
+        newTaskForm.appendChild(newTaskInput);
+        newTaskForm.appendChild(newTaskLabel);
+        newTaskForm.appendChild(newTaskIcon);
+
+        newTaskA.append(newTaskForm);
+        list.appendChild(newTaskA);
+
+        event.preventDefault();
+        todoList.push(newTaskA);
+
+        document.querySelector('#textarea1').value = '';
 
 
-});
+    });
 
-list.addEventListener ('click', (event) => {
-    let idPerformTask = event.target.htmlFor;
-    if(event.target.tagName === 'LABEL') {
-        if (event.target.style.textDecoration === "line-through") {
-            event.target.style = "text-decoration:none";
-        } else {
-            event.target.style = "text-decoration:line-through";
+    list.addEventListener('click', (event) => {
+        let idPerformTask = event.target.htmlFor;
+        if (event.target.tagName === 'LABEL') {
+            if (event.target.style.textDecoration === 'line-through') {
+                event.target.style = 'text-decoration:none';
+            } else {
+                event.target.style = 'text-decoration:line-through';
+            }
         }
-    };
+        ;
 
-    // event.preventDefault();
-    // console.log(event);
+        if (event.target.tagName === 'I') {
+            for (key in todoList) {
+                for (jey in todoList[key]) {
+                    if (event.target.parentNode.parentNode.className.split(" ")[1] === todoList[key].className.split(" ")[1]) {
+                        delete todoList[key];
+                        let elem = event.target.parentNode.parentNode;
+                        elem.remove();
+                    }
+                    ;
+                }
+                ;
+            }
+            ;
+        }
+        ;
+    });
 
-    if(event.target.tagName === 'I') {
-        // console.log(event.target.tagName);
-        for(key in todoList) {
-            for(jey in todoList[key]) {
-                if(event.target.parentNode.parentNode.className.split(" ")[1] === todoList[key].className.split(" ")[1]) {
-                    // console.log('todoList[key]:', todoList[key]);
-                    delete todoList[key];
-                    let elem = event.target.parentNode.parentNode;
-                    elem.remove();
-                };
-            };
-        };
-        // console.log(event.target.parentNode.parentNode.className.split(" ")[1]);
-        // console.log('console.log(todoList);', todoList);
-    };
-});
-
+})();
